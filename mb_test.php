f42360e584648042b0d682a8308fe8c1b9501b44
Cal Henderson <cal@iamcal.com>
2009-10-04 21:52:49 +0000

    more tests!
    git-svn-id: https://svn.iamcal.com/public/php/lib_sanitize/trunk@4797 ac7c8259-0de0-4f3c-bb6a-12394caf8efd

--- mb_test.php
+++ mb_test.php
@@ -9,21 +9,22 @@
 	# make sure we filter out bytes that are never valid UTF-8
 	#
 	# C0-C1 - overlong encoding
-	# F5-FD - start of 5-6 bytes sequences
+	# F5-F7 - 4 byte with code point over U+10FFFF
+	# F8-FD - start of 5-6 bytes sequences
 	# FE-FF - not used
 	#
 
 	test_string("hello\xC0world", "helloworld", "overlong encoding byte C0");
 	test_string("hello\xC1world", "helloworld", "overlong encoding byte C1");
-	test_string("hello\xF5world", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xF6world", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xF7world", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xF8world", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xF9world", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xFAworld", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xFBworld", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xFCworld", "helloworld", "start of deprecated 5-6 byte sequences");
-	test_string("hello\xFDworld", "helloworld", "start of deprecated 5-6 byte sequences");
+	test_string("hello\xF5world", "helloworld", "start of 4 byte code point over U+10FFFF");
+	test_string("hello\xF6world", "helloworld", "start of 4 byte code point over U+10FFFF");
+	test_string("hello\xF7world", "helloworld", "start of 4 byte code point over U+10FFFF");
+	test_string("hello\xF8world", "helloworld", "start of deprecated 5 byte sequence");
+	test_string("hello\xF9world", "helloworld", "start of deprecated 5 byte sequence");
+	test_string("hello\xFAworld", "helloworld", "start of deprecated 5 byte sequence");
+	test_string("hello\xFBworld", "helloworld", "start of deprecated 5 byte sequence");
+	test_string("hello\xFCworld", "helloworld", "start of deprecated 6 byte sequence");
+	test_string("hello\xFDworld", "helloworld", "start of deprecated 6 byte sequence");
 	test_string("hello\xFEworld", "helloworld", "invalid byte FE");
 	test_string("hello\xFFworld", "helloworld", "invalid byte FF");
 
@@ -60,6 +61,24 @@
 
 
 	#
+	# encoding that are out of range
+	#
+	# 4 bytes over U+10FFFF
+	# 5 bytes
+	# 6 bytes
+	#
+
+	test_string(c8(0x110000), "", "lowest out of range 4-byte U+110000");
+	test_string(c8(0x1FFFFF), "", "highest out of range 4-byte U+1FFFFF");
+
+	test_string("\xF8\x88\x80\x80\x80", "", "lowest 5-byte U+200000");
+	test_string("\xFB\xBF\xBF\xBF\xBF", "", "highest 5-byte U+3FFFFFF");
+
+	test_string("\xFC\x84\x80\x80\x80\x80", "", "lowest 6-byte U+4000000");
+	test_string("\xFD\xBF\xBF\xBF\xBF\xBF", "", "highest 6-byte U+7FFFFFFF");
+
+
+	#
 	# check we remove overlong encodings (using 3 bytes when only 2 were needed)
 	#
 	# 2 byte: 1100000x 10xxxxxx
@@ -74,10 +93,12 @@
 	test_string("a\xC1\xBFb", "ab", "highest overlong 2-byte");
 
 	test_string("a\xE0\x80\x80b", "ab", "lowest overlong 3-byte");
-	test_string("a\xE0\x9F\xBFb", "ab", "lowest overlong 3-byte");
+	test_string("a\xE0\x9F\xBFb", "ab", "highest overlong 3-byte");
 
 	test_string("a\xF0\x80\x80\x80b", "ab", "lowest overlong 4-byte");
-	test_string("a\xF0\x8F\xBF\xBFb", "ab", "lowest overlong 4-byte");
+	test_string("a\xF0\x8F\xBF\xBFb", "ab", "highest overlong 4-byte");
+
+	# TODO: overlong 5 & 6 bytes
 
 
 	#
